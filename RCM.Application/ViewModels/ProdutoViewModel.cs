2f76f77351260210d0c21bb7e5834e9218270c8c
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-04-25 18:50:41 -0300

    Change ProdutoViewModel to match with Produto properties.

--- RCM.Application/ViewModels/ProdutoViewModel.cs
+++ RCM.Application/ViewModels/ProdutoViewModel.cs
@@ -15,10 +15,32 @@ namespace RCM.Application.ViewModels
         [StringLength(100, MinimumLength = 4, ErrorMessage = "O campo nome deve ter entre 5 e 100 caracteres.")]
         public string Nome { get; set; }
 
+        [Display(Name = "Referência do Fabricante")]
+        [StringLength(25, MinimumLength = 0, ErrorMessage = "O campo referência do fabricante deve ter entre até 25 caracteres.")]
+        public string ReferenciaFabricante { get; set; }
+
+        [Display(Name = "Referência Original")]
+        [StringLength(25, MinimumLength = 0, ErrorMessage = "O campo referência original deve até 25 caracteres.")]
+        public string ReferenciaOriginal { get; set; }
+
+        [Display(Name = "Referência Auxiliar")]
+        [StringLength(50, MinimumLength = 0, ErrorMessage = "O campo referência auxiliar deve até 50 caracteres.")]
+        public string ReferenciaAuxiliar { get; set; }
+
         [Display(Name = "Estoque")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo estoque é requerido.")]
         [Range(0, 9999, ErrorMessage = "A quantidade no estoque deve estar em um formato válido.")]
         public int Estoque { get; set; }
+
+        [Display(Name = "Estoque Mínimo")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo estoque mínimo é requerido.")]
+        [Range(0, 9999, ErrorMessage = "A quantidade mínima no estoque deve estar em um formato válido.")]
+        public int EstoqueMinimo { get; private set; }
+
+        [Display(Name = "Estoque Ideal")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo estoque ideal é requerido.")]
+        [Range(0, 9999, ErrorMessage = "A quantidade ideal no estoque deve estar em um formato válido.")]
+        public int EstoqueIdeal { get; private set; }
 
         [Display(Name = "Preço de Venda")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo preço de venda é requerido.")]
