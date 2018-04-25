c1f72a1112916ed89c4a04be6f7f44e4251b3e50
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-04-25 18:32:39 -0300

    Add new Produto properties do ProdutoCommand.

--- RCM.Domain/Commands/ProdutoCommands/ProdutoCommand.cs
+++ RCM.Domain/Commands/ProdutoCommands/ProdutoCommand.cs
@@ -7,7 +7,12 @@ namespace RCM.Domain.Commands.ProdutoCommands
     {
         public Guid Id { get; set; }
         public string Nome { get; set; }
+        public string ReferenciaFabricante { get; set; }
+        public string ReferenciaOriginal { get; set; }
+        public string ReferenciaAuxiliar { get; set; }
         public int Estoque { get; set; }
+        public int EstoqueMinimo { get; set; }
+        public int EstoqueIdeal { get; set; }
         public decimal PrecoVenda { get; set; }
         public Guid MarcaId { get; set; }
     }
