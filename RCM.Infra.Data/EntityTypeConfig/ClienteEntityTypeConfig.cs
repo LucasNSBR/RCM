635dfa1759531aa3184fd83ae7e673d62380b770
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-04-29 18:57:54 -0300

    Configure Endereco owned type on Cliente.

--- RCM.Infra.Data/EntityTypeConfig/ClienteEntityTypeConfig.cs
+++ RCM.Infra.Data/EntityTypeConfig/ClienteEntityTypeConfig.cs
@@ -22,23 +22,49 @@ namespace RCM.Infra.Data.EntityTypeConfig
             {
                 cfg.Property(co => co.Celular)
                     .HasMaxLength(15)
-                    .HasColumnName("Celular");
+                    .HasColumnName("ContatoCelular");
 
                 cfg.Property(co => co.Email)
                     .HasMaxLength(100)
-                    .HasColumnName("Email");
+                    .HasColumnName("ContatoEmail");
 
                 cfg.Property(co => co.TelefoneComercial)
                     .HasMaxLength(15)
-                    .HasColumnName("TelefoneComercial");
+                    .HasColumnName("ContatoTelefoneComercial");
 
                 cfg.Property(co => co.TelefoneResidencial)
                     .HasMaxLength(15)
-                    .HasColumnName("TelefoneResidencial");
+                    .HasColumnName("ContatoTelefoneResidencial");
 
                 cfg.Property(co => co.Observacao)
                     .HasMaxLength(250)
                     .HasColumnName("ContatoObservacao");
+            });
+
+            builder.OwnsOne(e => e.Endereco, cfg =>
+            {
+                cfg.Property(en => en.Numero)
+                    .IsRequired()
+                    .HasMaxLength(4)
+                    .HasColumnName("EnderecoNumero");
+
+                cfg.Property(en => en.Rua)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .HasColumnName("EnderecoRua");
+
+                cfg.Property(en => en.Bairro)
+                    .IsRequired()
+                    .HasMaxLength(25)
+                    .HasColumnName("EnderecoBairro");
+
+                cfg.Property(en => en.Complemento)
+                    .HasMaxLength(250)
+                    .HasColumnName("EnderecoComplemento");
+
+                cfg.Property(en => en.CEP)
+                    .HasMaxLength(8)
+                    .HasColumnName("EnderecoCEP");
             });
         }
     }
