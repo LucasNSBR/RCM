e9a919b2d7d044da595b883b8e80214d8f7e54d1
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-06-01 18:32:10 -0300

    Migrate to latest database.

---
+++ RCM.Infra.Data/Migrations/20180601211519_ProdutoFornecedorReferencia.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RCM.Infra.Data.Migrations
+{
+    public partial class ProdutoFornecedorReferencia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenciaFornecedor",
+                table: "ProdutoFornecedor",
+                maxLength: 25,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferenciaFornecedor",
+                table: "ProdutoFornecedor");
+        }
+    }
+}
